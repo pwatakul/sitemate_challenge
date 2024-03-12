10800b6290f29f56b5865334cf46054be93fea4b
patawat watakul <patawat.wtk@gmail.com>
2024-03-12 12:27:02 +0700

    init the react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function dashboard() {
+
+  return (
+    <div>
+      <h1>Issue dashboard</h1>
+
+  </div>
+  );
+}
