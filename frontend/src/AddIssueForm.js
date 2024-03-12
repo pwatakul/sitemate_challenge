d724e015e590ab405cdf38e4bc290698a61599ec
patawat watakul <patawat.wtk@gmail.com>
2024-03-12 12:52:56 +0700

    Just clean all code and have been check all functions. It ready to submit

--- frontend/src/AddIssueForm.js
+++ frontend/src/AddIssueForm.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function AddObjectForm({ onAdd }) {
   const [title, setTitle] = useState('');
